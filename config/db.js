faa4ad818c9d6fcc46d175ff1b64f9342648f60a
Juan <pablogomez@gmail.com>
2023-02-28 23:31:45 -0300

    db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const conectarDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        const url = `${connection.connection.host}:${connection.connection.port}`
-    } catch (error) {
-        console.log(`db.js: error: ${error.message}`)
-        process.exit(1)
-    }
-}
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-export default conectarDB
+    const url = `${connection.connection.host}:${connection.connection.port}`;
+    console.log(`MongoDB Conectado en: ${url} `);
+  } catch (error) {
+    console.log(`db.js error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default conectarDB;
